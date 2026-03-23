f8790f1f9961c2bb7eb84341d47c9a5ef5cc2931
shakilkhan95 <th.shakilkhan@gmail.com>
2026-03-23 06:00:00 +0400

    logo, navs, and icons styles added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { FiSearch, FiShoppingCart } from 'react-icons/fi';
+import { IoIosContact } from 'react-icons/io';
 
 const Header = () => {
     return (
-        <header className=''>
+        <header className='flex justify-between items-center p-5 md:px-10 lg:px-10 bg-[#C4DBCE]'>
             {/* logo section  */}
             <div>
-                <h3>FarmRoot</h3>
+                <h3 className='text-xl font-bold cursor-pointer'>FarmRoot</h3>
             </div>
 
             {/* nav section  */}
-            <nav>
-                <a href="/">Home</a>
-                <a href="/">Menu</a>
-                <a href="/">About Us</a>
-                <a href="/">Subscription</a>
-                <a href="/">Recipes</a>
-                <a href="/">Contact Us</a>
+            <nav className='flex gap-6'>
+                <a href="/" className='navHover'>Home</a>
+                <a href="/" className='navHover'>Menu</a>
+                <a href="/" className='navHover'>About Us</a>
+                <a href="/" className='navHover'>Subscription</a>
+                <a href="/" className='navHover'>Recipes</a>
+                <a href="/" className='navHover'>Contact Us</a>
+                
+            {/* icons section  */}
+            <div className='flex gap-2 ml-8'>
+                <span className='icons hover:bg-green-500 hover:text-white cursor-pointer'><FiSearch/></span>
+                <span className='icons hover:bg-green-500 hover:text-white cursor-pointer'><IoIosContact/></span>
+                <span className='icons hover:bg-green-500 hover:text-white cursor-pointer'><FiShoppingCart/></span>
+            </div>
             </nav>
+
         </header>
     );
 };
